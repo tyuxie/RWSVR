64824c4b7fa39edf4fa5f2726b2c67befb27f50c
Tianyu Xie <xtymath@163.com>
2024-09-08 16:44:56 +0800

    update README

--- README.md
+++ README.md
@@ -1,2 +1,57 @@
-# Improving Tree Probability Estimation with Stochastic Optimization and Variance Reduction
-This is the codebase for 'Improving Tree Probability Estimation with Stochastic Optimization and Variance Reduction'.
+### <div align="center"><font size=5>Improving Tree Probability Estimation with Stochastic Optimization and Variance Reduction</font><div> 
+
+<div align="center">Tianyu Xie*, Musu Yuan*, Minghua Deng, Cheng Zhang</div>
+
+## Installation
+This repository is a light CPU-based implementation of the paper titled "Improving Tree Probability Estimation with Stochastic Optimization and Variance Reduction".
+To create the python environment, use the following Anaconda command:
+```
+conda env create -f environment.yml
+```
+
+
+
+## Experiments on Tree Topology Probability Estimation
+
+**Simulation**  You can use the following command to reproduce the simulation studies (e.g., SEMVR, $\beta=0.004$, $K=1500$, $T=1000$):
+```
+cd ./SBN/simulation
+python main.py --method SEMVR --beta 0.004 --n_trees 1500 --k_all 1000
+```
+Here, the "--method" argument can take string value in "EM, SEM, SEMVR, SGA, SVRG". For ablation studies, you can freely specify the Dirichlet parameter ($\beta$) in "--beta", the number of trees in the training set ($K$) in "--n_trees", and the number of iterations per epoch ($T$) in "--k_all".
+
+**Real Data** You can use the following command to reproduce the experiments on real data (e.g., SEMVR on DS1, repo1):
+```
+cd ./SBN/real-data
+python main.py --method SEMVR --dataset 1 --repo 1 --moniter
+```
+Here, the "--method" argument can take string value in "EM, SEM, SEMVR, SGA, SVRG", the "--dataset" argument can take integer value from 1 to 8, and the "--repo" argument can take integer value from 1 to 10.
+Please pay attention to the learning rates for different methods when running the code, by specifying the "--ema_rate" for EM-based methods and "--lr" for gradient based methods.
+
+
+## Experiments on Variational Bayesian Phylogenetic Inference (VBPI)
+
+**Simulation** You can use the following command to reproduce the results of simulation studies on VBPI:
+```
+cd ./VBPI/simulation
+python main.py --method rwsvr --nParticle 10
+```
+Here, the "--method" argument can take string value in "vimco, rws, rwsvr". The "--nParticle" argument ($R$) can be alternatively specified, but a value of 10 is suggested.
+
+**Real Data** You can use the following command to reproduce the experiments on real data (e.g., DS1):
+```
+cd ./VBPI/real-data
+python main.py --dataset DS1 --nParticle 10 --maxIter 200000 ##VIMCO
+python main.py --dataset DS1 --rws --nParticle 10 --maxIter 200000 ##RWS
+python main.py --dataset DS1 --rwsvr --nParticle 10 --maxFIter 2000 --biter 100 --batch_F 1000 ##RWSVR
+```
+For the RWSVR method, the learning schedule is controlled by the number of epochs ("--maxFIter"), the number of iterations per epoch ($T$, "--biter"), and the epoch sample size ($F$, "--batch_F").
+You can also use the "--empFreq" argument to moniter the KL divergence to the ground truth during the training.
+
+### References
+- Zhang, Cheng, and Frederick A. Matsen IV. "Generalizing tree probability estimation via Bayesian networks." Advances in neural information processing systems 31 (2018).
+- Zhang, Cheng. "Improved variational Bayesian phylogenetic inference with normalizing flows." Advances in neural information processing systems 33 (2020): 18760-18771.
+
+--- 
+
+If you find this codebase useful, please consider citing our work:
--- SBN/simulation/main.py
+++ SBN/simulation/main.py
@@ -16,8 +16,8 @@ def get_args():
     parser = argparse.ArgumentParser()
     parser.add_argument('--beta', default=0.004, type=float)
     parser.add_argument('--n_trees', default=500, type=int)
-    parser.add_argument('--k_all', default=500, type=int)
-    parser.add_argument('--method', default='sbn-em', choices=['sbn-em', 'sbn-sem', 'sbn-semvr', 'sbn-sga', 'sbn-svrg', 'sbn-em-a', 'sbn-sem-a', 'sbn-semvr-a', 'freq'])
+    parser.add_argument('--k_all', default=1000, type=int)
+    parser.add_argument('--method', default='EM', choices=['EM', 'SEM', 'SEMVR', 'SGA', 'SVRG'])
     parser.add_argument('--emp',default=False, action='store_true', help='use the empirical probability')
     parser.add_argument('--niters', default=200, type=int)
     args = parser.parse_args()
@@ -48,46 +48,31 @@ def main(args):
     tree_wts = list(tree_wts / np.sum(tree_wts))
     rootsplit_supp_dict, subsplit_supp_dict = get_support_from_mcmc(taxa, tree_dict, tree_names, tree_wts)
     
-    if args.method == 'sbn-em':
+    if args.method == 'EM':
         model = SBN(taxa, rootsplit_supp_dict, subsplit_supp_dict, emp_tree_freq)
         logp, kldivs = model.EM_learn(tree_dict, tree_names, tree_wts, maxiter=args.niters, miniter=args.niters, abstol=1e-05, monitor=True, start_from_uniform=False, report_kl=True, alpha=0.0)
         results = {'logp': logp, 'kldivs': kldivs, 'size': len(tree_dict)}
-        save_to_path = 'results/simulation_{}/sbn-em/'.format(args.k_all)+'beta_'+str(args.beta)+'_k_'+str(args.n_trees)+'.pkl'
-    if args.method == 'sbn-sem':
+        save_to_path = 'results/simulation_{}/EM/'.format(args.k_all)+'beta_'+str(args.beta)+'_k_'+str(args.n_trees)+'.pkl'
+    if args.method == 'SEM':
         model = SBN(taxa, rootsplit_supp_dict, subsplit_supp_dict, emp_tree_freq)
         logp, kldivs = model.SEMVR_learn(tree_dict, tree_names, tree_wts, maxiter=args.niters, miniter=args.niters, monitor=True, ema_rate=0.999, start_from_uniform=False, biter=args.k_all, alpha=0.0, report_kl=True)
         results = {'logp': logp, 'kldivs': kldivs, 'size': len(tree_dict)}
-        save_to_path = 'results/simulation_{}/sbn-sem/'.format(args.k_all)+'beta_'+str(args.beta)+'_k_'+str(args.n_trees)+'.pkl'
-    if args.method == 'sbn-semvr':
+        save_to_path = 'results/simulation_{}/SEM/'.format(args.k_all)+'beta_'+str(args.beta)+'_k_'+str(args.n_trees)+'.pkl'
+    if args.method == 'SEMVR':
         model = SBN(taxa, rootsplit_supp_dict, subsplit_supp_dict, emp_tree_freq)
         logp, kldivs = model.SEMVR_learn(tree_dict, tree_names, tree_wts, maxiter=args.niters, miniter=args.niters, monitor=True, ema_rate=0.99, start_from_uniform=False, biter=args.k_all, alpha=0.0, report_kl=True, vr=True)
         results = {'logp': logp, 'kldivs': kldivs, 'size': len(tree_dict)}
-        save_to_path = 'results/simulation_{}/sbn-semvr/'.format(args.k_all)+'beta_'+str(args.beta)+'_k_'+str(args.n_trees)+'.pkl'
-    if args.method == 'sbn-sga':
+        save_to_path = 'results/simulation_{}/SEMVR/'.format(args.k_all)+'beta_'+str(args.beta)+'_k_'+str(args.n_trees)+'.pkl'
+    if args.method == 'SGA':
         model = SBN(taxa, rootsplit_supp_dict, subsplit_supp_dict, emp_tree_freq)
         logp, kldivs = model.SGA_learn(0.0001, tree_dict, tree_names, tree_wts, batch=1, maxiter=args.niters, miniter=args.niters, abstol=1e-05, biter=args.k_all, ar=0.75, af=10, start_from_uniform=False, monitor=True, report_kl=True, vr=False)
         results = {'logp': logp, 'kldivs': kldivs, 'size': len(tree_dict)}
-        save_to_path = 'results/simulation_{}/sbn-sga/'.format(args.k_all)+'beta_'+str(args.beta)+'_k_'+str(args.n_trees)+'.pkl'
-    if args.method == 'sbn-svrg':
+        save_to_path = 'results/simulation_{}/SGA/'.format(args.k_all)+'beta_'+str(args.beta)+'_k_'+str(args.n_trees)+'.pkl'
+    if args.method == 'SVRG':
         model = SBN(taxa, rootsplit_supp_dict, subsplit_supp_dict, emp_tree_freq)
         logp, kldivs = model.SGA_learn(0.001, tree_dict, tree_names, tree_wts, batch=1, maxiter=args.niters, miniter=args.niters, abstol=1e-05, biter=args.k_all, ar=0.75, af=10, start_from_uniform=False, monitor=True, report_kl=True, vr=True)
         results = {'logp': logp, 'kldivs': kldivs, 'size': len(tree_dict)}
-        save_to_path = 'results/simulation_{}/sbn-svrg/'.format(args.k_all)+'beta_'+str(args.beta)+'_k_'+str(args.n_trees)+'.pkl'
-    if args.method == 'sbn-em-a':
-        model = SBN(taxa, rootsplit_supp_dict, subsplit_supp_dict, emp_tree_freq)
-        logp, kldivs = model.EM_learn(tree_dict, tree_names, tree_wts, maxiter=args.niters, miniter=args.niters, abstol=1e-05, monitor=True, start_from_uniform=False, report_kl=True, alpha=50.0/args.n_trees)
-        results = {'logp': logp, 'kldivs': kldivs, 'size': len(tree_dict)}
-        save_to_path = 'results/simulation_{}/sbn-em-a/'.format(args.k_all)+'beta_'+str(args.beta)+'_k_'+str(args.n_trees)+'.pkl'
-    if args.method == 'sbn-sem-a':
-        model = SBN(taxa, rootsplit_supp_dict, subsplit_supp_dict, emp_tree_freq)
-        logp, kldivs = model.SEMVR_learn(tree_dict, tree_names, tree_wts, maxiter=args.niters, miniter=args.niters, monitor=True, ema_rate=0.999, start_from_uniform=False, biter=args.k_all, alpha=50.0/args.n_trees, report_kl=True)
-        results = {'logp': logp, 'kldivs': kldivs, 'size': len(tree_dict)}
-        save_to_path = 'results/simulation_{}/sbn-sem-a/'.format(args.k_all)+'beta_'+str(args.beta)+'_k_'+str(args.n_trees)+'.pkl'
-    if args.method == 'sbn-semvr-a':
-        model = SBN(taxa, rootsplit_supp_dict, subsplit_supp_dict, emp_tree_freq)
-        logp, kldivs = model.SEMVR_learn(tree_dict, tree_names, tree_wts, maxiter=args.niters, miniter=args.niters, monitor=True, ema_rate=0.99, start_from_uniform=False, biter=args.k_all, alpha=50.0/args.n_trees, report_kl=True, vr=True)
-        results = {'logp': logp, 'kldivs': kldivs, 'size': len(tree_dict)}
-        save_to_path = 'results/simulation_{}/sbn-semvr-a/'.format(args.k_all)+'beta_'+str(args.beta)+'_k_'+str(args.n_trees)+'.pkl'
+        save_to_path = 'results/simulation_{}/SVRG/'.format(args.k_all)+'beta_'+str(args.beta)+'_k_'+str(args.n_trees)+'.pkl'
 
     with open(save_to_path, 'wb') as f:
         pickle.dump(results, f)
